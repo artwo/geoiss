f37e1cdb88fd6bb6336556eaeedf2836f4e41df1
Arturo <arturoarl294@gmail.com>
2020-11-30 22:37:27 +0100

    Remove unused functionality in GeometryService.kt

--- src/main/kotlin/geoiss/service/GeometryService.kt
+++ src/main/kotlin/geoiss/service/GeometryService.kt
@@ -1,17 +1,11 @@
 package geoiss.service
 
 import geoiss.model.geojson.GeoCountry
-import geoiss.model.geojson.Polygon
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class GeometryService @Autowired constructor(
 //    val geoCities: List<GeoCity>,
-//    val geoCountries: CountriesGeoJson,
     val geoCountries: List<GeoCountry>,
-    private val polygonsById: Map<String, Polygon>
-) {
-
-    fun polygonById(id: String): Polygon? = polygonsById[id]
-}
+)
